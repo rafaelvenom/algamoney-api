bb24a70e3ab37a507518c92c5db65c306b77f511
Rafael Oliveira <rafael-azevedo@outlook.com>
2021-02-27 22:16:27 -0300

    alterada url de revogação do token

--- src/app/seguranca/logout.service.ts
+++ src/app/seguranca/logout.service.ts
@@ -1,17 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { environment } from './../../environments/environment';
 import { AuthService } from './auth.service';
 
 @Injectable()
 export class LogoutService {
 
-  tokensRenokeUrl = 'http://localhost:8080/tokens/revoke';
+  tokensRenokeUrl: string;
 
   constructor(
     private http: HttpClient,
     private auth: AuthService
-  ) { }
+  ) {
+    this.tokensRenokeUrl = `${environment.apiUrl}/tokens/revoke`;
+  }
 
   logout() {
     return this.http.delete(this.tokensRenokeUrl, { withCredentials: true })
